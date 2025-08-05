72ed7525a94f6f9f0aac14bd66d5d3488138ba18
Sheno Bhai <shenobhai786@gmail.com>
2025-08-05 16:52:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Source_Code_Pro } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
+import { Button } from "@/components/ui/button";
 
 import {
   Book,
